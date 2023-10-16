aeac304377522b8071cefd7e4548869a98a97092
John William <johnwill.v.2017@gmail.com>
2023-10-16 20:07:47 -0300

    fix

--- backend/src/main/resources/db/migration/V2__create_table_acting_area.sql
+++
@@ -1,6 +0,0 @@
-
-CREATE TABLE IF NOT EXISTS `acting_areas` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
